9cd973a2ed74d5fef17a9d0c064796766e76ff60
Margaret Kosmala <mkosmala@gmail.com>
2014-12-12 11:43:31 -0500

    Initial set up of algorithm files; much still to do

---
+++ karger_algorithm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# I think I will do this for Season 4, which we have gold standard data for.
+# Remove expert answers? Probably should...
+
+# Read in a file of solutions in the format:
+# Capture,User,Species
+
+
+# Go through and pick a species. Transform file so that there is one
+# line for each Capture-User and the Answer is 1 if they said that species
+# is there and -1 if not.
+
+
+
+
